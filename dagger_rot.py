78c045851802f2b81a3a8bc3b95bd97d391703b3
EdoardoZappia <edoardozappia5@gmail.com>
2025-05-28 11:51:15 +0200

    m

--- dagger_rot.py
+++ dagger_rot.py
@@ -111,7 +111,7 @@ def load_agents(checkpoint_path_rot, env=None):
 
     state_dim_rot = 2    # theta, theta_target
 
-    agent_rot = DDPGRotAgent(state_dim_rot, 1)
+    agent_rot = DDPGAgent(state_dim_rot, 1)
 
     ckpt_rot = torch.load(checkpoint_path_rot, map_location=torch.device('cpu'))
 
@@ -129,9 +129,7 @@ if __name__ == "__main__":
     # Istanzia ambiente ed esperto
     env = TrackingEnv()
 
-    expert_model = DDPGAgent(input_dim, output_dim)
     expert_model = load_agents("Rotazioni-dinamiche/No-noise/ddpg_rot_0.01_20250509_163508/checkpoint_ep782.pth", env)
-    expert_model.load_state_dict(torch.load("Rotazioni-dinamiche/No-noise/ddpg_mov_0.01_20250509_163508/checkpoint_ep782.pth"))
     expert_model.eval()
 
     agent_model = PolicyNetwork(input_dim, output_dim)
