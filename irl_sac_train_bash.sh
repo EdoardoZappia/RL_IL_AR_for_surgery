b142d55574b27956465b420f7c79e4068fe1886a
EdoardoZappia <edoardozappia5@gmail.com>
2025-06-08 19:25:57 +0200

    m

---
+++ irl_sac_train_bash.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=bash
+#SBATCH --partition=GPU
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+#SBATCH --time=2:00:00
+#SBATCH --gpus-per-node=1
+#SBATCH --mem=100G
+#SBATCH --output=job_%j.out
+#SBATCH --error=job_%j.err
+
+module load anaconda
+source activate mujoco-env
+
+echo "Inizio training IRL"
+python train_irl_sac.py
