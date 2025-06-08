330d1d145b3de93d21526b00f9e1de3d52cdfad6
EdoardoZappia <edoardozappia5@gmail.com>
2025-06-08 12:30:55 +0200

    m

--- irl_sac_train.py
+++ irl_sac_train.py
@@ -249,7 +249,7 @@ for iter in range(1000):
     # 3. Aggiorna la policy ogni 5 iterazioni
     if iter % 5 == 0:
         print(">>> Aggiorno la policy con SAC")
-        agent.learn(total_timesteps=10000)
+        agent.learn(total_timesteps=1000)
 
 # Salva il reward appreso
 torch.save(reward_net.state_dict(), "IL/DME_SAC/reward_network_rot_0.5_0.01.pt")
