34d0a73d8a7c179c333cf2076c1ddca4644ab5cb
EdoardoZappia <edoardozappia5@gmail.com>
2025-06-08 12:34:30 +0200

    m

--- irl_sac_train.py
+++ irl_sac_train.py
@@ -229,11 +229,20 @@ for iter in range(1000):
     policy_obs, policy_act = [], []
     obs, _ = env.reset()
     for _ in range(1000):
+        # act, _ = agent.predict(obs.reshape(1, -1), deterministic=True)
+        # new_obs, _, done, _, _ = env.step(act[0])
+        # policy_obs.append(obs)
+        # policy_act.append(act[0])
+        # obs = new_obs if not done else env.reset()[0]
         act, _ = agent.predict(obs.reshape(1, -1), deterministic=True)
-        new_obs, _, done, _, _ = env.step(act[0])
+        new_obs, _, done, truncated, _ = env.step(act[0])
         policy_obs.append(obs)
         policy_act.append(act[0])
-        obs = new_obs if not done else env.reset()[0]
+        
+        obs = new_obs
+        if truncated:  # Episodio finito
+            obs, _ = env.reset()
+            
     policy_obs = np.array(policy_obs).squeeze()
     policy_act = np.array(policy_act).squeeze()
 
