d4cebcdbf28a34204d2e7350d0b3e58beebd11a8
EdoardoZappia <edoardozappia5@gmail.com>
2025-06-10 10:09:48 +0200

    m

---
+++ irl_td3_train_bash.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=bash
+#SBATCH --partition=GPU
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+#SBATCH --time=2:00:00
+#SBATCH --gpus-per-node=1
+#SBATCH --mem=100G
+#SBATCH --output=job_%j.out
+#SBATCH --error=job_%j.err
+
+echo "Inizio training IRL"
+python irl_td3_rot_train.py
