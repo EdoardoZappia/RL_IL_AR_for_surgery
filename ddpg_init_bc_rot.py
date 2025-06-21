09401809322b16e203bb1a097edaadef76cd6441
EdoardoZappia <edoardozappia5@gmail.com>
2025-06-21 19:00:00 +0200

    m

--- ddpg_init_bc_rot.py
+++ ddpg_init_bc_rot.py
@@ -194,7 +194,7 @@ def train_ddpg(env=None, num_episodes=10001):
         real_state = torch.tensor(state, dtype=torch.float32).to(device)
         state = torch.tensor(state, dtype=torch.float32).to(device)
         state = state.clone()
-        state[1:] += torch.normal(mean=0.0, std=0.005, size=(1,), device=state.device)
+        state[1:] += torch.normal(mean=0.0, std=0.004, size=(1,), device=state.device)
 
         agent.noise_std = max(agent.min_noise_std, agent.noise_std * agent.noise_decay)
         trajectory, target_trajectory = [], []
@@ -213,7 +213,7 @@ def train_ddpg(env=None, num_episodes=10001):
             real_next_state = torch.tensor(next_state, dtype=torch.float32).to(device)
             next_state = torch.tensor(next_state, dtype=torch.float32).to(device)
             next_state = next_state.clone()
-            next_state[1:] += torch.normal(mean=0.0, std=0.005, size=(1,), device=next_state.device)
+            next_state[1:] += torch.normal(mean=0.0, std=0.004, size=(1,), device=next_state.device)
 
             if torch.norm(real_next_state[0] - real_state[1]) < tolerance:
                 total_attached_counter += 1
