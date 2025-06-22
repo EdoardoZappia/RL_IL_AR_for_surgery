6e9c3ac6cb92c9f246ed589d6f5abced4999530e
EdoardoZappia <edoardozappia5@gmail.com>
2025-06-22 15:08:56 +0200

    m

--- ddpg_bc_KL_transl.py
+++ ddpg_bc_KL_transl.py
@@ -26,7 +26,7 @@ GAMMA = 0.99
 TAU = 0.005
 EARLY_STOPPING_EPISODES = 50
 CHECKPOINT_INTERVAL = 100
-PRETRAIN_CRITIC_EPISODES = 100
+PRETRAIN_CRITIC_EPISODES = 0
 
 now = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
 RUN_DIR = f"Esperimento_1_corretto/KL/Traslazioni-dinamiche/ddpg_mov_0.05_std_0.005_{now}"
