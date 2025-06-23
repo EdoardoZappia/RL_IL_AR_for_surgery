e43c3c3425ec24f45b6e71fa5b2f0995b44dfdcf
EdoardoZappia <edoardozappia5@gmail.com>
2025-06-23 13:19:18 +0200

    m

--- BC_training.py
+++ BC_training.py
@@ -15,7 +15,7 @@ class TrajectoryDataset(Dataset):
     def __getitem__(self, idx):
         return self.observations[idx], self.actions[idx]
 
-dataset = TrajectoryDataset("trajectories_correct/dataset_rot_std_0.004.npz")
+dataset = TrajectoryDataset("trajectories_correct/dataset_transl_std_0.005.npz")
 dataloader = DataLoader(dataset, batch_size=64, shuffle=True)
 
 class BCModel(torch.nn.Module):
@@ -31,7 +31,7 @@ class BCModel(torch.nn.Module):
         action = torch.tanh(self.fc3(x)) * 5.0
         return action
 
-def train_bc_model(model, dataloader, num_epochs=100, learning_rate=1e-3, device=None, save_path="IL/BC_dataset_correct/bc_policy_rot_0.5_0.01_std_0.004.pth"):
+def train_bc_model(model, dataloader, num_epochs=200, learning_rate=1e-3, device=None, save_path="IL/BC_dataset_correct/bc_policy_transl_0.2_0.05_std_0.005.pth"):
 
     # Crea la cartella se non esiste
     os.makedirs(os.path.dirname(save_path), exist_ok=True)
@@ -67,5 +67,5 @@ def train_bc_model(model, dataloader, num_epochs=100, learning_rate=1e-3, device
     print(f"Modello salvato in '{save_path}'")
 
 if __name__ == "__main__":
-    model = BCModel(input_dim=2, output_dim=1)
+    model = BCModel(input_dim=4, output_dim=2)
     train_bc_model(model, dataloader)
