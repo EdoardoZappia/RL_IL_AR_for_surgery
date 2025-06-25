6c17822d36bbe70a8fef72cf2782859c021acf88
EdoardoZappia <edoardozappia5@gmail.com>
2025-06-25 15:02:10 +0200

    m

--- ddpg_bc_KL_transl.py
+++ ddpg_bc_KL_transl.py
@@ -29,7 +29,7 @@ CHECKPOINT_INTERVAL = 100
 PRETRAIN_CRITIC_EPISODES = 0
 
 now = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-RUN_DIR = f"Esperimento_1_corretto/KL/Traslazioni-dinamiche/ddpg_mov_0.05_std_0.005_buffer_pieno_no_init_exp_{now}"
+RUN_DIR = f"Esperimento_1_corretto/KL/Traslazioni-dinamiche/ddpg_mov_0.05_std_0.006_buffer_pieno_no_init_exp_{now}"
 os.makedirs(RUN_DIR, exist_ok=True)
 
 class PolicyNet(nn.Module):
@@ -252,7 +252,7 @@ def train_ddpg(env=None, num_episodes=10001):
         real_state = torch.tensor(state, dtype=torch.float32).to(device)
         state = torch.tensor(state, dtype=torch.float32).to(device)
         state = state.clone()
-        state[2:4] += torch.normal(mean=0.0, std=0.005, size=(2,), device=device)
+        state[2:4] += torch.normal(mean=0.0, std=0.006, size=(2,), device=device)
 
         agent.noise_std = max(agent.min_noise_std, agent.noise_std * agent.noise_decay)
         trajectory, target_trajectory = [], []
@@ -273,7 +273,7 @@ def train_ddpg(env=None, num_episodes=10001):
             real_next_state = torch.tensor(next_state, dtype=torch.float32).to(device)
             next_state = torch.tensor(next_state, dtype=torch.float32).to(device)
             next_state = next_state.clone()
-            next_state[2:4] += torch.normal(mean=0.0, std=0.005, size=(2,), device=device)
+            next_state[2:4] += torch.normal(mean=0.0, std=0.006, size=(2,), device=device)
 
             if torch.norm(real_next_state[:2] - real_state[2:4]) < tolerance:
                 total_attached_counter += 1
