9fb2fa29705c6c97d5c7c019ee3cfa90dd4bfc1b
EdoardoZappia <edoardozappia5@gmail.com>
2025-06-25 12:46:29 +0200

    m

--- ddpg_bc_KL_rot.py
+++ ddpg_bc_KL_rot.py
@@ -26,10 +26,10 @@ GAMMA = 0.99
 TAU = 0.005
 EARLY_STOPPING_EPISODES = 50
 CHECKPOINT_INTERVAL = 100
-PRETRAIN_CRITIC_EPISODES = 0 #100
+PRETRAIN_CRITIC_EPISODES = 100
 
 now = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-RUN_DIR = f"Esperimento_1_corretto/KL/Rotazioni-dinamiche/ddpg_mov_0.01_std_0.004_buffer_pieno_pre-tr_exp_PROVA_{now}"
+RUN_DIR = f"Esperimento_1_corretto/KL/Rotazioni-dinamiche/ddpg_mov_0.01_std_0.004_buffer_pieno_pre-tr_exp_frozen_{now}"
 #RUN_DIR = f"TEST_NOISE/Rotazioni-dinamiche/ddpg_mov_0.01_std_0.004_{now}"
 os.makedirs(RUN_DIR, exist_ok=True)
 
